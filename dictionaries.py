151bfe2ba0407667d45500a4625c419408e113a0
daigavane70 <daigavane70@gmail.com>
2021-04-01 15:41:52 +0530

    disctionaries updated

---
+++ dictionaries.py
@@ -0,0 +1,26 @@
+#---------------------------------------------------------
+
+dict = {'name': "Vedant Daigavane", 'branch': "Information Technology", 'mobile': '9822927562', 'div': "SE-9"}
+
+#get keys
+print("keys: ", dict.keys())
+
+#get values
+print("value: ", dict.values())
+
+#value linked to a key
+print( dict['name'] )
+
+#-----------------------------------------------------------
+
+#creating a nested dictionary
+dict2 = { 'college': "PICT", 'student': {'name': 'Vedant Daigavane', 'roll no': "23116", "email": "daigavane70@gmail.com"} }
+
+print( dict2['student']['roll no'] )
+
+#-----------------------------------------------------------
+
+y = [ dict2['student'][k] for k in dict2['student'] ]
+print( y )
+
+#-----------------------------------------------------------
