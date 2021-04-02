1b67bb45d08eee55681c4b3193e78ed091867c68
daigavane70 <daigavane70@gmail.com>
2021-04-02 09:20:00 +0530

    functions

---
+++ functions.py
@@ -0,0 +1,31 @@
+def my_func( a = 12 ):
+    
+    #this will print valur of a to console
+    print(a)
+
+    #this will return 2*a to the place where the function is called
+    return 2*a
+
+my_func()   #will print default value 12
+my_func(44) #will print 44
+
+print( my_func(100) )   #this will print 200 which is returned by the function 'my_func'
+
+def addFunc( a, b ):
+
+    return a+b
+
+print( addFunc(12,18) ) #this will print 30 as addition of 12 and 18
+
+"""
+Now Lets validate our result using the 'type' keyword
+"""
+
+def addFunc(a, b, c):
+    if type(a) == type(b) == type(c):        # the 'type' keyword returns the data type of the variable ex: type(12) -> <class 'int'>
+        return a+b+c
+    else:
+        print("Sorrry Invalid Arguments")
+
+print(addFunc(1,1,1))       #this will print the expected result 
+print(addFunc(1,1,'a'))     #here we will get the 
